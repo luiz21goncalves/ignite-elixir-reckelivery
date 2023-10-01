a508cde8e5fe0a2bfb2255cfaa310944c18c657e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-01 11:08:48 -0300

    chore: add validations and generate password hash to user schema

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -1,11 +1,15 @@
 defmodule Rockelivery.User do
   use Ecto.Schema
 
+  alias Ecto.Changeset
+
   import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
-  @required_params [:age, :address, :cep, :cpf, :email, :password_hash, :name]
+  @required_params [:age, :address, :cep, :cpf, :email, :password, :name]
+
+  @mail_regex ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
 
   schema "users" do
     field :age, :integer
@@ -13,6 +17,7 @@ defmodule Rockelivery.User do
     field :cep, :string
     field :cpf, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :name, :string
 
@@ -22,5 +27,22 @@ defmodule Rockelivery.User do
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_length(:age, greater_then_or_equal: 18)
+    |> validate_format(:email, @mail_regex)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    password_hash = Pbkdf2.hash_pwd_salt(password)
+
+    change(changeset, %{password_hash: password_hash})
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end

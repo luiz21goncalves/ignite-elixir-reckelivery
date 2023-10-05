8feeb451450efbb37c6daa5c587114e5ea1c03d3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-05 07:27:00 -0300

    test: create user

---
+++ test/reckelivery/users/create_test.exs
@@ -0,0 +1,41 @@
+defmodule Reckelivery.Users.CreateTest do
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Rockelivery.Users.Create
+  alias Rockelivery.{Error, User}
+
+  describe "call/1" do
+    test "when all params are valid, returns the user" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok,
+              %User{
+                age: 25,
+                address: "Rua das bananeiras, 15",
+                cep: "12345678",
+                cpf: "12345678901",
+                email: "email@banana.com",
+                password: "123456",
+                name: "João das Bananeiras"
+              }} = response
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = build(:user_params, %{age: 10, password: "123"})
+
+      response = Create.call(params)
+
+      expected_response = %{
+        password: ["should be at least 6 character(s)"],
+        age: ["must be greater than or equal to 18"]
+      }
+
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end

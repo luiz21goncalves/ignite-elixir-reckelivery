3cb9a809cfd2a13a9c5303854a39e1930ce3a690
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-11 20:07:46 -0300

    feat: add route to create order

--- lib/rockelivery_web/router.ex
+++ lib/rockelivery_web/router.ex
@@ -15,6 +15,8 @@ defmodule RockeliveryWeb.Router do
       resources "/users", UsersController, except: [:new, :edit]
 
       post "/items", ItemsController, :create
+
+      post "/orders", OrdersController, :create
     end
   end
 

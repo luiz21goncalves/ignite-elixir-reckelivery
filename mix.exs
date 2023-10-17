4b6159300ba5be7a61f4c13b896eea0fa3fbeae8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-17 07:55:14 -0300

    chore: add mox dependency

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Rockelivery.MixProject do
       {:ex_machina, "~> 2.7.0", only: :test},
       {:tesla, "~> 1.7"},
       {:hackney, "~> 1.17"},
-      {:bypass, "~> 2.1", only: :test}
+      {:bypass, "~> 2.1", only: :test},
+      {:mox, "~> 1.1", only: :test}
     ]
   end
 

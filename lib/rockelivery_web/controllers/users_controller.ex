3b155c5488409ec755e221f5409dbcee6afa7de1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-22 14:48:31 -0300

    feat: generate jwt when creating user

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -2,15 +2,17 @@ defmodule RockeliveryWeb.UsersController do
   use RockeliveryWeb, :controller
 
   alias Rockelivery.User
+  alias RockeliveryWeb.Auth.Guardian
   alias RockeliveryWeb.FallbackController
 
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
+    with {:ok, %User{} = user} <- Rockelivery.create_user(params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
       conn
       |> put_status(:created)
-      |> json(%{user: user})
+      |> json(%{user: user, token: token})
     end
   end
 

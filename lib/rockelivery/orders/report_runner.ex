784643bdfcf655b0cb5247313873a85d83685d71
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-11-04 09:38:49 -0300

    chore: add report runner module

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,37 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rockelivery.Orders.Report
+
+  def start_link(_initial_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    Logger.info("init")
+
+    schedule_report_generation()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:generate, state) do
+    Logger.info("Generating report ...")
+
+    Report.create()
+
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    one_day_in_milliseconds = 1000 * 60 * 60 * 24
+
+    Process.send_after(self(), :generate, one_day_in_milliseconds)
+  end
+end

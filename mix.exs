f55511c0384810cab6cec514cfc1bf671992a1f1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-11-05 09:53:30 -0300

    ci: add coverage and codecov steps

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Rockelivery.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.json": :test
       ]
     ]
   end
